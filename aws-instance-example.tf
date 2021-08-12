e6216a9a93366b45d2c70a7d1c5f0e176b6a4168
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-12 16:59:23 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -2,15 +2,15 @@ resource "aws_instance" "web1" {
    ami           = "ami-0c2b8ca1dad447f8a"
    instance_type = "t2.micro"
    count = 1
-  vpc_security_group_ids = ["sg-0990c12803c100850"]
+  vpc_security_group_ids = ["sg-01718400c2741bdb3"]
    key_name               = "Linux_Terraform-Chef"     
    
    user_data = <<-EOF
-		#! /bin/bash
-                sudo apt-get update
-		sudo apt-get install -y apache2
-		sudo systemctl start apache2
-		sudo systemctl enable apache2
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+		#!/bin/bash
+                yum update -y
+		yum install -y httpd.x86_64
+		systemctl start httpd.service
+		systemctl enable httpd.service
+		echo "Hello World from $(hostname -f)" > /var/www/html/index.html
 	EOF
 }
