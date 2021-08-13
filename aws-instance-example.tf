9dc95da79e2c4a5a76ea48b7faae34434c5017a9
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-13 11:29:17 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -16,6 +16,18 @@ resource "aws_instance" "web1" {
 		wget -O /tmp/chef.rpm https://packages.chef.io/files/stable/chef-workstation/20.7.96/el/7/chef-workstation-20.7.96-1.el7.x86_64.rpm
                 sudo rpm -Uvh /tmp/chef.rpm
                 curl -L https://omnitruck.chef.io/install.sh | sudo bash -s -- -v 15.8.23
+		sudo yum update -y
+       		sudo yum install git -y
+       		git version
+       		mkdir mygit
+       		cd mygit
+       		git clone https://github.com/Abishek-Ravichander/Stater_Kit.git
+       		cd Stater_Kit
+       		unzip chef-starter
+       		mv apache-cookbook chef-repo
+       		cd chef-repo
+       		mv apache-cookbook cookbooks
+       		cd cookbooks
 	EOF
 
   
