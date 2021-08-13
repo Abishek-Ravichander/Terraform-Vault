dced71c7327c66d18c3690387e579452621dd69c
Abishek-Ravichander <70654020+Abishek-Ravichander@users.noreply.github.com>
2021-08-13 10:24:20 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -5,6 +5,15 @@ resource "aws_instance" "web1" {
   vpc_security_group_ids = ["sg-01718400c2741bdb3"]
    key_name               = "Linux_Terraform-Chef" 
    iam_instance_profile =   "myManagedInstanceRole"
-   
+   user_data = <<-EOF
+		#!/bin/bash
+                sudo mkdir /tmp/ssm
+		cd /tmp/ssm
+		wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+		sudo dpkg -i amazon-ssm-agent.deb
+		sudo systemctl enable amazon-ssm-agent
+		rm amazon-ssm-agent.deb
+	EOF
+
   
 }
